44740043103ae103804104a30c73d9fb0587a5d8
Сергей Савин <sergius8482@gmail.com>
2026-05-04 16:23:05 +0300

    lesson-3

---
+++ lesson-3.js
@@ -0,0 +1,39 @@
+// Задачи: арифметика.
+
+// Сумма трех чисел
+// let num1 = parseInt(prompt("Число 1: "));
+// let num2 = parseInt(prompt("Число 2: "));
+// let num3 = parseInt(prompt("Число 3: "));
+
+// console.log(num1 + num2 + num3);
+
+// Следующее и предыдущее
+// let n = 20;
+
+// if (n < 20) {
+//   console.log(`Для числа ${n} предыдущее число: ${n - 1}`);
+// } else if (n === 20) {
+//   console.log(`Следующее за числом ${n} число: ${n + 1}`);
+// }
+
+// Мандарины
+// const n = parseInt(prompt("Школьников: ")); // 10
+// const k = parseInt(prompt("Мандаринов: ")); // 25
+
+// console.log(`Каждому: ${Math.floor(k / n)}`); // 2
+// console.log(`В корзине ${k % n}`); // 5
+
+// Сумма цифр трехзначного числа
+// const num = 333;
+// n1 = num % 10; // 3 - извлекаем единицы
+// n2 = Math.floor(num / 10) % 10; // 3 - извлекаем десятки
+// n3 = Math.floor(num / 100); // 3 - извлекаем сотни
+
+// const resultSum = n1 + n2 + n3;
+// const resultProd = n1 * n2 * n3;
+
+// console.log(`Сумма цифр = ${resultSum}`);
+// console.log(`Произведение цифр = ${resultProd}`);
+
+// console.log(resultSum);
+// console.log(resultProd);
