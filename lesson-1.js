909b70d0ae057d8e24cd7cc87935c1af975c2e72
Сергей Савин <sergius8482@gmail.com>
2026-05-04 16:20:20 +0300

    lesson-1

---
+++ lesson-1.js
@@ -0,0 +1,45 @@
+// Задачи: вывод данных.
+
+// Три строки
+// console.log("JavaScript");
+// console.log("это");
+// console.log("круто!");
+
+// Паспорт программиста
+// let firstName = prompt("Имя");
+// let language = prompt("Язык");
+// let year = prompt("Год");
+// let company = prompt("Компания");
+
+// console.log("Имя: " + firstName);
+// console.log("Язык: " + language);
+// console.log("Год: " + year);
+// console.log("Компания: " + company);
+
+// Рамка из символов
+// console.log("***** \n*   * \n*****");
+// console.log("*****");
+// console.log("*   *");
+// console.log("*****");
+
+// Пустые строки
+// console.log("Раз");
+// console.log(''); // если указываю кавычки как аргумент, то пустая строка вставляется.
+// console.log("Два");
+// console.log('');
+// console.log("Три");
+
+// Полное имя
+// let firstName = prompt("Имя");
+// let surname = prompt("Фамилия");
+// console.log(`${firstName} ${surname}`);
+// console.log(firstName + " " + surname);
+
+// Визитка
+// let firstName2 = prompt("Имя");
+// let age = prompt("Возраст");
+// let language = prompt("Язык");
+
+// console.log(`Имя: ${firstName2}`);
+// console.log(`Возраст: ${age}`);
+// console.log(`Язык: ${language}`);
