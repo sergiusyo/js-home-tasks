819ef3d366bd8a8d261d7fcbd53febaa23ceebf8
Сергей Савин <sergius8482@gmail.com>
2026-05-04 16:22:03 +0300

    lesson-2

---
+++ lesson-2.js
@@ -0,0 +1,38 @@
+// Задачи: переменные.
+
+// Привет со стилем
+// let firstName = "Брендан";
+// let city = "Москва";
+
+// console.log(`Привет, ${firstName}! Ты из ${city}?`);
+
+// Константа курса
+const COURSE = "JavaScript";
+// console.log(`Я изучаю: ${COURSE}`);
+// console.log("Я изучаю: " + COURSE);
+
+// Исправь имена
+// let firstName = "Аня";
+// let myScore = 100;
+// let lang = "JS";
+
+// Обмен значений
+// let a = 5;
+// let b = 10;
+// console.log(b, a);
+
+// Счетчик
+// let count = 0;
+// count += 1;
+// count += 1;
+// count += 1;
+// console.log(count);
+
+// Автобиография
+// let firstName = prompt("Имя?");
+// let age = prompt("Возраст?");
+// let city = prompt("Город?");
+
+// console.log("Имя: " + firstName);
+// console.log("Возраст: " + age);
+// console.log("Город: " + city);
