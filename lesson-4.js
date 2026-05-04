95a14aaed82d1274de740cd77aab1793add8a71a
Сергей Савин <sergius8482@gmail.com>
2026-05-04 16:24:04 +0300

    lesson-4

---
+++ lesson-4.js
@@ -0,0 +1,73 @@
+// Задачи: if...else.
+
+// Положительное?
+// let n = Number(prompt("Число?"));
+// if (n > 0) {
+//   console.log("Положительное");
+// } else if (n < 0) {
+//   console.log("Отрицательное");
+// } else {
+//   console.log("Ноль");
+// }
+
+// Максимум из двух
+// let a = 7;
+// let b = 3;
+
+// if (a > b) {
+//   console.log(`${a}`);
+// } else {
+//   console.log(`${b}`);
+// }
+
+// let result = a > b ? a : b;
+// console.log(result);
+
+// FizzBuzz
+// let n = Number(prompt("Число")); // 15
+
+// if (n % 15 === 0) {
+//   console.log("FizzBuzz");
+// } else if (n % 3 === 0) {
+//   console.log("Fizz");
+// } else if (n % 5 === 0) {
+//   console.log("Buzz");
+// } else {
+//   console.log(n);
+// }
+
+// Оценка
+// let n = Number(prompt("Балл"));
+// if (n >= 90) {
+//   console.log("A");
+// } else if (n >= 80) {
+//   console.log("B");
+// } else if (n >= 70) {
+//   console.log("C");
+// } else if (n >= 60) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// Високосный год
+// let year = Number(prompt("Какой год?"));
+
+// if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+//   console.log('Високосный!')
+// } else {
+//   console.log('Не високосный!')
+// }
+
+// Сезон
+// let month = Number(prompt("Какой месяц?"));
+
+// if (month >= 3 && month <= 5) {
+//   console.log("Весна");
+// } else if (month >= 6 && month <= 8) {
+//   console.log("Лето");
+// } else if (month >= 9 && month <= 11) {
+//   console.log("Осень");
+// } else {
+//   console.log("Зима");
+// }
