50110825078ae67eb33fa8488a08fe8a76764ef4
Сергей Савин <sergius8482@gmail.com>
2026-05-08 13:39:59 +0300

    lesson-6

--- lesson-6.js
+++ lesson-6.js
@@ -1,2 +1,49 @@
-// Задачи: типы и конвертация
-// Тип переменной
+// Задачи: типы и конвертация.
+
+// Тип переменнойю.
+// let n = 42;
+// let s = "Hello";
+// let bool = true;
+// let u = undefined;
+
+// console.log(typeof n);
+// console.log(typeof s);
+// console.log(typeof bool);
+// console.log(typeof u);
+
+// Конвертация.
+// let str = prompt("Число:");
+// let num = Number(s);
+// console.log(typeof str);
+// console.log(typeof num);
+
+// Максимум из трёх.
+// let n = Math.max(3, 7, 5);
+// console.log(n);
+
+// let n1 = prompt("Число 1:");
+// let n2 = prompt("Число 2:");
+// let n3 = prompt("Число 3:");
+
+// console.log(Math.max(n1, n2, n3));
+
+// Содержит ли?
+// let text = prompt("Текст");
+// let sub = prompt("Что ищем?");
+
+// console.log(text.includes(sub) ? "Найдено" : "Не найдено");
+
+// Задачи: abs и строки.
+
+// Сумма по модулю.
+// let n1 = Number(prompt("Число 1:"));
+// let n2 = Number(prompt("Число 2:"));
+// let n3 = Number(prompt("Число 3:"));
+
+// console.log(Math.abs(n1) + Math.abs(n2) + Math.abs(n3));
+
+// Анализ строки
+// let str = prompt("Введите строку!");
+// console.log(str.length);
+// console.log(str.toUpperCase());
+// console.log(str.includes("a"));
