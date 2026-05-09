f5754fed16a29d768f40af5d18da7baa30fdba71
Сергей Савин <sergius8482@gmail.com>
2026-05-09 20:52:22 +0300

    lesson-7

--- lesson-7.js
+++ lesson-7.js
@@ -1,10 +1,19 @@
 // Задачи: базовый цикл for.
 // Таблица умножения.
 
-const n = 3;
+// const n = 3;
 
-for (let i = 1; i <= 10; i++) {
-  console.log(`${n} x ${i} = ${n * i}`);
-}
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${n} x ${i} = ${n * i}`);
+// }
 
 // Звёздный прямоугольник
+
+// const w = parseInt(prompt("Ширина:"));
+// const h = parseInt(prompt("Высота:"));
+
+// for (let i = 0; i < h; i++) {
+//   console.log("*".repeat(w));
+// }
+
+// Сумма от 1 до N
