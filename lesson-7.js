d61434f993e98facdb560cdc03e012ff41bb6065
Сергей Савин <sergius8482@gmail.com>
2026-05-10 10:48:13 +0300

    lesson-7

--- lesson-7.js
+++ lesson-7.js
@@ -17,3 +17,23 @@
 // }
 
 // Сумма от 1 до N
+// let sum = parseInt(prompt("Число:"));
+
+// for (let i = 1; i < 10; i++) {
+//   sum += i; // 9 итераций
+// }
+
+// console.log(sum);
+
+// Объяснение.
+// sum = 10 + (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+// sum = 10 + 45;
+// sum = 55;
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     sum += i // 10 итераций
+// }
+
+// console.log(sum)
