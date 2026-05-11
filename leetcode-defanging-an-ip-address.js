91c1a1f7a05aed953c36a2cfd327df4abb08201a
Сергей Савин <sergius8482@gmail.com>
2026-05-11 16:19:04 +0300

    leetcode-defanging-of-ip-address

--- leetcode-defanging-an-ip-address.js
+++ leetcode-defanging-an-ip-address.js
@@ -1,1 +1,6 @@
 // Если указан действительный (IPv4) IP-адрес, верните исправленную версию этого IP-адреса.
+var defangIPaddr = function (address) {
+  return address.replaceAll(".", "[.]");
+};
+
+console.log(defangIPaddr("255.100.50.0")); // 1[.]1[.]1[.]1
