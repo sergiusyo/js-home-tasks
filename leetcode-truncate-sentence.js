6e5a63c5c88931cbdc65300227944c7890d30901
Сергей Савин <sergius8482@gmail.com>
2026-05-11 17:08:11 +0300

    leetcode-truncate-sentence

--- leetcode-truncate-sentence.js
+++ leetcode-truncate-sentence.js
@@ -1,1 +1,9 @@
-// 
+var truncateSentence = function (s, k) {
+  return s.split(" ", 5).join(" ");
+};
+
+console.log(
+  truncateSentence(
+    "chopper is not a tanuki is the solution to this problem",
+  ),
+);
