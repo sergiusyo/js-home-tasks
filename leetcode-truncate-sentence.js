a45810d326a08c29a9b3b98f14909a618dabb596
Сергей Савин <sergius8482@gmail.com>
2026-05-15 21:07:59 +0300

    leetcode-truncate-sentence

--- leetcode-truncate-sentence.js
+++ leetcode-truncate-sentence.js
@@ -1,11 +1,11 @@
-var truncateSentence = function (s, k) {
-  // console.log(k);
-  return s.split(" ", k).join(" ");
-};
+// var truncateSentence = function (s, k) {
+// console.log(k);
+//   return s.split(" ", k).join(" ");
+// };
 
-console.log(
-  truncateSentence(
-    "chopper is not a tanuki is the solution to this problem",
-    3,
-  ),
-);
+// console.log(
+//   truncateSentence(
+//     "chopper is not a tanuki is the solution to this problem",
+//     3,
+//   ),
+// );
