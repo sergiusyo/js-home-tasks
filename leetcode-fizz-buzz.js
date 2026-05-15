a1b32263da9d9c6e0aa7a126611a118fb3eb81ac
Сергей Савин <sergius8482@gmail.com>
2026-05-15 21:11:37 +0300

    leetcode-fizz-buzz

--- leetcode-fizz-buzz.js
+++ leetcode-fizz-buzz.js
@@ -1,21 +1,21 @@
 // fizz-buzz
-var fizzBuzz = function (n) {
-  const result = []; // Создаём массив для хранения строк
-  for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      result.push("FizzBuzz");
-    } else if (i % 3 === 0) {
-      result.push("Fizz");
-    } else if (i % 5 === 0) {
-      result.push("Buzz");
-    } else {
-      result.push(i.toString());
-    }
-  }
-  return result;
-};
+// var fizzBuzz = function (n) {
+//   const result = []; // Создаём массив для хранения строк
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       result.push("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       result.push("Fizz");
+//     } else if (i % 5 === 0) {
+//       result.push("Buzz");
+//     } else {
+//       result.push(i.toString());
+//     }
+//   }
+//   return result;
+// };
 
 // Пример использования:
-console.log(fizzBuzz(3));
-console.log(fizzBuzz(5));
-console.log(fizzBuzz(15));
+// console.log(fizzBuzz(3));
+// console.log(fizzBuzz(5));
+// console.log(fizzBuzz(15));
