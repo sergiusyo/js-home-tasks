58348f22ee6fc1ea39f8c272ac399a2ce338ade8
Сергей Савин <sergius8482@gmail.com>
2026-05-16 17:27:14 +0300

    leetcode-sum-multiples

--- leetcode-sum-multiples.js
+++ leetcode-sum-multiples.js
@@ -1,2 +1,25 @@
 // Цикл от 1 до n, внутри проверка % 3, % 5, % 7. Нужно аккуратно не посчитать одно число дважды через ||.
-var sumOfMultiples = function (n) {};
+// var sumOfMultiples = function (n) {
+//   let sum = 0;
+//   for (let i = 1; i < n; i++) {
+//     if ((i % 3 === 0 && i % 5 === 0) || i % 7 !== 0) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// };
+
+var sumOfMultiples = function (n) {
+  let sum = 0;
+  for (let i = 1; i < n; i++) {
+    // Новое условие: число кратно 3 или 5 ИЛИ не делится на 7
+    if (i % 3 === 0 || i % 5 === 0 || i % 7 !== 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+console.log(sumOfMultiples(7)); // 21
+console.log(sumOfMultiples(10)); // 40
+console.log(sumOfMultiples(9)); // 30
