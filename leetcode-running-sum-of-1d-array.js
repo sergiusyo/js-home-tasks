df6930d3d082960e7b1e533c3fd0c48042598f73
Сергей Савин <sergius8482@gmail.com>
2026-05-16 12:19:58 +0300

    running-sum-of-1d-array

--- leetcode-running-sum-of-1d-array.js
+++ leetcode-running-sum-of-1d-array.js
@@ -1,1 +1,11 @@
-var runningSum = function (nums) {};
+// Задан массив nums. Текущая сумма массива определяется как
+// runningSum[i] = sum(nums[0]...nums[i]). Вернуть текущую сумму nums.
+
+var runningSum = function (nums) {
+  for (let i = 1; i < nums.length; i++) {
+    nums[i] += nums[i - 1]; // На каждом шаге к текущему элементу прибавляется значение предыдущего.
+  }
+  return nums;
+};
+
+console.log(runningSum([1, 2, 3, 4]));
