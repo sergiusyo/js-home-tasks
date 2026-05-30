487413b6ea5cf0c13782f1b9a86bc7b46a377f2c
Сергей Савин <sergius8482@gmail.com>
2026-05-30 10:35:54 +0300

    lesson-11

--- lesson-11.js
+++ lesson-11.js
@@ -1,2 +1,20 @@
 // Задачи: вложенные циклы.
 // Квадрат из *
+
+// let n = Number(prompt("Число:"));
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= n; j++) {
+//     row += "* ";
+//   }
+//   console.log(row);
+// }
+
+// let n = Number(prompt('Введи n:'))
+// for (let i = 0; i < n; i++) {
+//   console.log('* '.repeat(n).trim())
+// }
+
+// ==================================
+
+// Шахматная доска.
