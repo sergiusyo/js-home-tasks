11e33a4132f5be0f0aa210c63c082495a6d434a7
Сергей Савин <sergius8482@gmail.com>
2026-05-31 17:33:43 +0300

    stars

--- stars.js
+++ stars.js
@@ -1,4 +1,10 @@
 // Квадрат из *. Спросите у пользователя число n. Выведите квадрат из звёздочек размером n×n.
-let n = Number(prompt("Число:"));
+// let n = Number(prompt("Число:"));
 
-for (let i = 1; i < n; i++) {}
+// for (let i = 1; i <= n; i++) {
+//   let row = "";
+//   for (let j = 1; j <= n; j++) {
+//     row += "* ";
+//   }
+//   console.log("* ".repeat(n).trim());
+// }
