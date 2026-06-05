65bcd7661846329686f073795ad163d5b149bdf0
Сергей Савин <sergius8482@gmail.com>
2026-06-05 16:02:24 +0300

    lesson-12

--- lesson-12.js
+++ lesson-12.js
@@ -1,11 +1,11 @@
 // Задачи: индексация строк. В столбик.
-// const s = prompt("Введите строку: ");
+// const s = prompt("Введите строку:");
 // for (let i = 0; i < s.length; i++) {
 //   console.log(`${i + 1}: ${s[i]}`);
 // }
 
 // Сколько раз?
-// const s = prompt("Строка:");
+// const s = prompt("Введите строку:");
 // const target = prompt("Символ:");
 // let count = 0;
 // for (let char of target) {
@@ -16,11 +16,15 @@
 // console.log(count);
 
 // Цифра 1.
-// const s = prompt("Строка:");
+// const s = prompt("Введите строку:");
 // for (const char of s) {
 //   if (char >= "0" && char <= "9") {
 //     console.log(char);
 //   }
 // }
 
-// Делаем срезы. 
+// Делаем срезы.
+// const s = prompt("Введите строку:");
+// console.log(s.slice(0, 3), s.slice(-3), s.slice(1, -1));
+
+// Палиндром. 
