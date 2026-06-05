dc5f2ee899e0a6e11d54ead87165bd53a10bd892
Сергей Савин <sergius8482@gmail.com>
2026-06-05 15:14:19 +0300

    lesson-12

--- lesson-12.js
+++ lesson-12.js
@@ -1,7 +1,18 @@
 // Задачи: индексация строк. В столбик.
-const s = prompt("Введите строку: ");
-for (let i = 0; i < s.length; i++) {
-  console.log(`${i + 1}: ${s[i]}`);
-}
+// const s = prompt("Введите строку: ");
+// for (let i = 0; i < s.length; i++) {
+//   console.log(`${i + 1}: ${s[i]}`);
+// }
 
 // Сколько раз?
+// const s = prompt("Строка:");
+// const target = prompt("Символ:");
+// let count = 0;
+// for (let char of target) {
+//   if (char === "a") {
+//     count++;
+//   }
+// }
+// console.log(count);
+
+// Цифра 1.
