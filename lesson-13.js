92b297404b837a460deb942a476975d865770941
Сергей Савин <sergius8482@gmail.com>
2026-06-07 15:32:24 +0300

    lesson-13

--- lesson-13.js
+++ lesson-13.js
@@ -1,1 +1,7 @@
 // Нормализация ввода.
+// let name = prompt("Введите имя:");
+// name = name.trim();
+// let cap = name[0].toUpperCase() + name.slice(1).toLowerCase();
+// console.log(`Привет, ${cap}!`);
+
+// Регистронезависимое сравнение.
