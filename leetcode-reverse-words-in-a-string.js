2ac113d74c32b234480203880db2098d82deb915
Сергей Савин <sergius8482@gmail.com>
2026-06-09 16:33:15 +0300

    leetcode-reverse-words-in-a-string

--- leetcode-reverse-words-in-a-string.js
+++ leetcode-reverse-words-in-a-string.js
@@ -1,10 +1,10 @@
-var reverseWords = function (s) {
-  // 1. Переворачиваем всю строку целиком
-  const reversedString = s.split("").reverse().join("");
+// var reverseWords = function (s) {
+//   // 1. Переворачиваем всю строку целиком
+//   const reversedString = s.split("").reverse().join("");
 
-  // 2. Разбиваем перевернутую строку на слова, переворачиваем их порядок и собираем обратно
-  const result = reversedString.split(" ").reverse().join(" ");
-  return result;
-};
+//   // 2. Разбиваем перевернутую строку на слова, переворачиваем их порядок и собираем обратно
+//   const result = reversedString.split(" ").reverse().join(" ");
+//   return result;
+// };
 
-console.log(reverseWords("Let's take LeetCode contest"));
+// console.log(reverseWords("Let's take LeetCode contest"));
