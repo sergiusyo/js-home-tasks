9c25c8ea696c1d06180bceb87149531b3e9b060f
Сергей Савин <sergius8482@gmail.com>
2026-06-12 18:03:16 +0300

    patterns

--- patterns.js
+++ patterns.js
@@ -20,6 +20,35 @@
 //   }
 // }
 
+// let arr = [54, 5, 6, 7];
+// let n = arr.length;
+// let left = 0;
+// let right = n - 1;
+
+// while (left < right) {
+//   [arr[left], arr[right]] = [arr[right], arr[left]];
+//   left++;
+//   right--;
+// }
+// console.log(left, right);
+
+// Перевернутая строка (палиндром через объявление функции).
+function reverseString(str) {
+  const arr = str.split("");
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    left++;
+    right--;
+  }
+
+  return arr.join("");
+}
+
+console.log(reverseString("hello"));
+
 // Пример.
 // const reverseString = (str) => {
 //   let arr = [...str]; // преобразуем строку в массив
