33d4b01d403ba2f7f178e68876b73707c6bffff5
Сергей Савин <sergius8482@gmail.com>
2026-06-13 10:24:48 +0300

    sliding-window

--- sliding-window.js
+++ sliding-window.js
@@ -5,3 +5,25 @@
 //    L        R
 
 // Подстроки, подмассивы.
+
+// Пример.
+// Максимальная сумма подмассива длины k.
+
+function maxSum(nums, k) { // количествр элементов подмножества
+  let windowSum = 0; // хранит теку сумму элементов в "окне" (подмножестве) длиной k
+  for (let i = 0; i < k; i++) {
+    windowSum += nums[i]; 
+  }
+
+  let result = windowSum;
+
+  for (let i = k; i < nums.length; i++) {
+
+    windowSum = windowSum + nums[i] - nums[i - k];
+
+    result = Math.max(result, windowSum);
+  }
+
+  return result;
+}
+
