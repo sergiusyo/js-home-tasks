be332671ade46b8e1c34e145a916f743e889b086
Сергей Савин <sergius8482@gmail.com>
2026-06-13 15:48:04 +0300

    two-pointers-home

--- two-pointers-home.js
+++ two-pointers-home.js
@@ -14,24 +14,34 @@ function reversedArray(arr) {
 }
 
 const myArray = [1, 2, 3, 4, 5];
-console.log("Исходный массив:", myArray);
+// console.log("Исходный массив:", myArray);
 
 reversedArray(myArray);
 console.log("Перевернутый массив:", myArray);
 
 // Является ли "level" палиндромом?
+// function reversedString(str) {
+//   const arr = str.split("");
+//   let left = 0;
+//   let right = arr.length - 1;
+
+//   while (left < right) {
+//     [arr[left], arr[right]] = [arr[right], arr[left]];
+//     left++;
+//     right--;
+//   }
+
+//   return arr.join("");
+// }
+
 function reversedString(str) {
   const arr = str.split("");
-  let left = 0;
-  let right = arr.length - 1;
-
-  while (left < right) {
+  for (let left = 0, right = arr.length - 1; left < right; left++, right--) {
     [arr[left], arr[right]] = [arr[right], arr[left]];
-    left++;
-    right--;
   }
-
   return arr.join("");
 }
 
 console.log(reversedString("level")); // палиндром!
+
+//
