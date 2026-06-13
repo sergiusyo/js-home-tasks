8d8825cf6279abd44524f66b6d49c8b42f45e35c
Сергей Савин <sergius8482@gmail.com>
2026-06-13 15:19:35 +0300

    two-pointers-home

--- two-pointers-home.js
+++ two-pointers-home.js
@@ -1,1 +1,20 @@
 // 🔥 Разминка — задачи для разогрева
+// Перевернуть массив через два указателя.
+
+function reversedArray(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+
+    left++;
+    right--;
+  }
+}
+
+const myArray = [1, 2, 3, 4, 5];
+console.log("Исходный массив:", myArray);
+
+reversedArray(myArray);
+console.log("Перевернутый массив:", myArray);
