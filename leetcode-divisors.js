ee60a5faa3fca98404b60a85401711478572516b
Сергей Савин <sergius8482@gmail.com>
2026-06-16 14:39:55 +0300

    leetcode-divisors

--- leetcode-divisors.js
+++ leetcode-divisors.js
@@ -16,34 +16,34 @@
 //     return true;
 //   }
 
-  // const sqrtN = Math.sqrt(n); // проверяет, является ли n полным квадратом
-  // Math.sqrt(4) = 2, Math.floor(2) = 2 //→ равны
-  // Math.sqrt(8) ≈ 2.828, Math.floor(2.828) = 2 //→ не равны
-  // return sqrtN === Math.floor(sqrtN) && isPrime(sqrtN); // isPrime(sqrtN) — проверяет, является ли корень простым числом
+// const sqrtN = Math.sqrt(n); // проверяет, является ли n полным квадратом
+// Math.sqrt(4) = 2, Math.floor(2) = 2 //→ равны
+// Math.sqrt(8) ≈ 2.828, Math.floor(2.828) = 2 //→ не равны
+// return sqrtN === Math.floor(sqrtN) && isPrime(sqrtN); // isPrime(sqrtN) — проверяет, является ли корень простым числом
 // }
 
 // console.log(isThree(4)); // true
 // console.log(isThree(2)); // false
 
 // Дано целое число num. Вернуть true, если это совершенное число (сумма собственных делителей = num).
-// function checkPerfectNumber(num) {
-//   if (num <= 1) return false; // 0, 1 и отриц. числа не м/б совершенными
+function checkPerfectNumber(num) {
+  if (num <= 1) return false; // 0, 1 и отриц. числа не м/б совершенными
 
-//   let sumOfDivisors = 0; // накопитель суммы всех собственных делителей
+  let sumOfDivisors = 0; // накопитель суммы всех собственных делителей
 
-//   // Ищем собственные делители (до num / 2)
-//   for (let i = 1; i <= num / 2; i++) {
-//     // num / 2 - у числа не м/б СД больше его половины (кроме его самого)
-//     // для 28 это 14 (28 / 2)
-//     if (num % i === 0) {
-//       // проверяет, делится ли num на i без остатка. Если да, то i
-//       // добавляется к сумме делителей
-//       sumOfDivisors += i;
-//     }
-//   }
+  // Ищем собственные делители (до num / 2)
+  for (let d = 1; d <= num / 2; d++) {
+    // num / 2 - у числа не м/б СД больше его половины (кроме его самого)
+    // для 28 это 14 (28 / 2)
+    if (num % d === 0) {
+      // проверяет, делится ли num на i без остатка. Если да, то i
+      // добавляется к сумме делителей
+      sumOfDivisors += d;
+    }
+  }
 
-//   return sumOfDivisors === num; // если сумма СД равна исходному числу - возвращает true
-//   // иначе false
-// }
+  return sumOfDivisors === num; // если сумма СД равна исходному числу - возвращает true
+  // иначе false
+}
 
-// console.log(checkPerfectNumber(28));   // true
+console.log(checkPerfectNumber(28)); // true
