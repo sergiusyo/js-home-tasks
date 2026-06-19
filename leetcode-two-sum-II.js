d77ffc725ba0beb2a1f6d1f95f14f1703ae1da7c
Сергей Савин <sergius8482@gmail.com>
2026-06-19 12:57:02 +0300

    Remove old file name two-sum-II.js

--- leetcode-two-sum-II.js
+++ leetcode-two-sum-II.js
@@ -1,1 +1,18 @@
-//
+// Найти два числа в отсортированном массиве numbers, которые в сумме дают target. 
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    // Вычисляется сумма чисел под указателями
+    const currentSum = numbers[left] + numbers[right];
+
+    if (currentSum === target) {
+      return [left + 1, right + 1]; // +1 потому что задача требует индексы с 1
+    } else if (currentSum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+};
