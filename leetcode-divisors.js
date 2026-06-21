4519cb500a0faa4bd60aadeeb7f640873e2364e4
Сергей Савин <sergius8482@gmail.com>
2026-06-21 11:37:32 +0300

    leetcode-divisors

--- leetcode-divisors.js
+++ leetcode-divisors.js
@@ -1,7 +1,7 @@
-// Three divisors. 
+// Three divisors.
 // Верните значение true, если n имеет ровно три положительных делителя.
 // В противном случае верните значение false.
-// 
+//
 // function isThree(n) {
 //   if (n < 4) return false; // наименьшее число с тремя делителями это 4
 
@@ -24,52 +24,57 @@
 // // return sqrtN === Math.floor(sqrtN) && isPrime(sqrtN); // isPrime(sqrtN) — проверяет, является ли корень простым числом
 // }
 
-var isThree = function (n) {
-  if (n === 1) {
-    return false;
-  }
+// var isThree = function (n) {
+//   if (n === 1) {
+//     return false;
+//   }
 
-  if (!Number.isInteger(Math.sqrt(n))) {
-    // число, НЕ являющееся квадратным корнем числа
-    return false;
-  }
+//   if (!Number.isInteger(Math.sqrt(n))) {
+//     // число, НЕ являющееся квадратным корнем числа
+//     return false;
+//   }
 
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i == 0 && i != Math.sqrt(n)) {
-      // если i делит исходное число n без остатка и
-      // при этом i не равно самому корню, то мы нашли нетривиальный делитель.
-      return false;
-    }
-  }
+//   for (let i = 2; i <= Math.sqrt(n); i++) {
+//     if (n % i == 0 && i != Math.sqrt(n)) {
+//       // если i делит исходное число n без остатка и
+//       // при этом i не равно самому корню, то мы нашли нетривиальный делитель.
+//       return false;
+//     }
+//   }
 
-  return true;
-};
+//   return true;
+// };
 
 // console.log(isThree(4)); // true
 // console.log(isThree(2)); // false
 // console.log(isThree(9)); // true
 // console.log(isThree(16)); // false
 
-// Perfect Number. 
-// Дано целое число num. Вернуть true, если это совершенное число (сумма собственных делителей = num).
+// Perfect Number.
+
 function checkPerfectNumber(num) {
-  if (num <= 1) return false; // 0, 1 и отриц. числа не м/б совершенными
-
-  let sumOfDivisors = 0; // накопитель суммы всех собственных делителей
-
-  // Ищем собственные делители (до num / 2)
-  for (let d = 1; d <= num / 2; d++) {
-    // num / 2 - у числа не м/б СД больше его половины (кроме его самого)
-    // для 28 это 14 (28 / 2)
-    if (num % d === 0) {
-      // проверяет, делится ли num на i без остатка. Если да, то i
-      // добавляется к сумме делителей
-      sumOfDivisors += d;
-    }
+  if (num < 2) {
+    return false;
   }
 
-  return sumOfDivisors === num; // если сумма СД равна исходному числу - возвращает true
-  // иначе false
+  let sumOfDivisors = 1; // единственный универсальный делитель
+
+  const sqrtN = Math.floor(Math.sqrt(num));
+
+  for (let i = 2; i <= sqrtN; i++) {
+    if (num % i === 0) {
+      sumOfDivisors += i;
+      // Находим парный делитель
+      const pairedDivisor = num / i;
+
+      // является ли парный делитель тем же самым числом
+      if (pairedDivisor !== i && pairedDivisor !== num) {
+        sumOfDivisors += pairedDivisor;
+      }
+    }
+  }
+  return sumOfDivisors === num;
 }
 
-// console.log(checkPerfectNumber(28)); // true
+console.log(checkPerfectNumber(28)); // true
+console.log(checkPerfectNumber(7)); // false
