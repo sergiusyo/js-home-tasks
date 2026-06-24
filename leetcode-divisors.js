cb3247ff99488dc41439523cd07071ea82a1e735
Сергей Савин <sergius8482@gmail.com>
2026-06-24 14:45:02 +0300

    leetcode-divisors

--- leetcode-divisors.js
+++ leetcode-divisors.js
@@ -133,7 +133,7 @@ var closestDivisors = function (num) {
   const pair1 = findClosestFactors(target1);
   const pair2 = findClosestFactors(target2);
 
-  // Сравниваем абсолютную разность элементов в парах. Math.abs() гарантирует, что разница всегда положительная
+  // Сравниваем абсолютную разность элементов в парах. Math.abs() игнорирует минус
   const diff1 = Math.abs(pair1[0] - pair1[1]); // разница diff1 = |3 - 3| = 0
   const diff2 = Math.abs(pair2[0] - pair2[1]); // разница diff2 = |2 - 5| = 3
 
