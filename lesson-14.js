bda0b7e24028efbc0bacb43aa390747fa8a1576a
Сергей Савин <sergius8482@gmail.com>
2026-06-25 15:17:51 +0300

    lesson-14

--- lesson-14.js
+++ lesson-14.js
@@ -1,5 +1,5 @@
 // Список покупок.
-let buyList = ["молоко", "яйца", "колбаса", "сыр", "хлеб"];
-console.log(buyList);
-console.log(buyList[0]);
-console.log(buyList[4]);
+let arr = ["молоко", "яйца", "колбаса", "сыр", "хлеб"];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[arr.length - 1]);
