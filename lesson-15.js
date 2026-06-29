9e8fcc6beab3f9b2356d37bd0bc5f09cdd53b507
Сергей Савин <sergius8482@gmail.com>
2026-06-29 14:14:54 +0300

    lesson-15

--- lesson-15.js
+++ lesson-15.js
@@ -1,1 +1,10 @@
 // Стек
+let stack = [];
+stack.push(prompt("Элемент 1:"));
+stack.push(prompt("Элемент 2:"));
+stack.push(prompt("Элемент 3:"));
+stack.pop();
+console.log(stack);
+
+// Очередь
+let array = [1, 2, 3, 4, 5];
