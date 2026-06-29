7dc327c12301f0b5d798c4fec8e60a11ef3f079f
Сергей Савин <sergius8482@gmail.com>
2026-06-29 12:48:48 +0300

    lesson-14

--- lesson-14.js
+++ lesson-14.js
@@ -1,10 +1,12 @@
-// Список покупок.
+// Список покупок
 let buyList = ["молоко", "яйца", "колбаса", "сыр", "хлеб"];
 console.log(buyList);
 console.log(buyList[0]);
 console.log(buyList[buyList.length - 1]);
 
-// Array.from строки
+// Array.from() строки
 let str = "Javascript";
 console.log(Array.from(str));        // ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
 console.log(Array.from(str).length); // 10
+
+// Задача 3: Очередь
