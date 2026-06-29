09be5b08565ea9b586ebc3cfaaa9da4964903899
Сергей Савин <sergius8482@gmail.com>
2026-06-29 14:38:58 +0300

    lesson-15

--- lesson-15.js
+++ lesson-15.js
@@ -7,9 +7,14 @@
 // console.log(stack);
 
 // Очередь
-let q = [1, 2, 3, 4, 5];
-console.log(q.shift());
-console.log(q.shift());
-console.log(q);
+// let q = [1, 2, 3, 4, 5];
+// console.log(q.shift());
+// console.log(q.shift());
+// console.log(q);
 
 // Вставить в середину
+let q = [1, 2, 4, 5];
+q.splice(2, 0, 3);
+console.log(q);
+
+// Топ-3
