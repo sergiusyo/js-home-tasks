3ef742462e254bcd83e368cc9c0e64b6f230bf6c
Сергей Савин <sergius8482@gmail.com>
2026-07-02 14:47:38 +0300

    lesson-14

--- lesson-14.js
+++ lesson-14.js
@@ -1,13 +1,13 @@
 // 📝 Задача 1: Список покупок
-let buyList = ["молоко", "яйца", "колбаса", "сыр", "хлеб"];
-console.log(buyList);
-console.log(buyList[0]);
-console.log(buyList[buyList.length - 1]);
+// let buyList = ["молоко", "яйца", "колбаса", "сыр", "хлеб"];
+// console.log(buyList);
+// console.log(buyList[0]);
+// console.log(buyList[buyList.length - 1]);
 
 // 📝 Задача 2: Array.from строки
-let str = "Javascript";
-console.log(Array.from(str)); // ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
-console.log(Array.from(str).length); // 10
+// let str = "Javascript";
+// console.log(Array.from(str)); // ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
+// console.log(Array.from(str).length); // 10
 
 // let str = "Javascript";
 // console.log(str.split('')); // ['J', 'a', 'v', 'a', 's', 'c', 'r', 'i', 'p', 't']
@@ -21,21 +21,21 @@ console.log(Array.from(str).length); // 10
 //   arr.push(char)
 // }
 
-// console.log(arr); 
-// console.log(arr.length); 
+// console.log(arr);
+// console.log(arr.length);
 
 // 📝 Задача 3: Очередь
-let firstName = ["Аня", "Боря", "Вася"];
-firstName.push("Галя");
-firstName.shift();
-console.log(firstName);
+// let firstName = ["Аня", "Боря", "Вася"];
+// firstName.push("Галя");
+// firstName.shift();
+// console.log(firstName);
 
 // 📝 Задача 4: Сумма массива
-let nums = [5, 10, 15, 20, 25];
-let sum = 0;
+// let nums = [5, 10, 15, 20, 25];
+// let sum = 0;
 
-for (const n of nums) {
-  sum += n;
-}
+// for (const n of nums) {
+//   sum += n;
+// }
 
-console.log(sum);
+// console.log(sum);
