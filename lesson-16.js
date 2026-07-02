18a533148f820f11166c8408ef4effc82ceb40f3
Сергей Савин <sergius8482@gmail.com>
2026-07-02 14:56:53 +0300

    lesson-16

--- lesson-16.js
+++ lesson-16.js
@@ -1,1 +1,9 @@
-//
+// Квадраты
+// let arr = [1, 2, 3, 4, 5]
+// console.log(arr.map(x => x * x))
+
+// Только длинные
+// let words = ["hi", "hello", "js", "javascript", "py"];
+// console.log(words.filter((w) => w.length > 3));
+
+// Сумма reduce
