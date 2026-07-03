811098967ed8d9686f6f2382dbbd02ee880e52d1
Сергей Савин <sergius8482@gmail.com>
2026-07-03 11:54:52 +0300

    leetcode-two-sum

--- leetcode-two-sum.js
+++ leetcode-two-sum.js
@@ -1,1 +1,11 @@
-//
+// Найти два числа в отсортированном массиве numbers, которые в сумме дают target.
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j]; // Возвращаем индексы найденной пары
+        }
+      }
+    }
+    return null 
+};
