6df1069199898bdca7277f2d5d65548bd4e78cc8
Сергей Савин <sergius8482@gmail.com>
2026-07-04 10:49:36 +0300

    leetcode-shuffle-the-array

--- leetcode-shuffle-the-array.js
+++ leetcode-shuffle-the-array.js
@@ -1,2 +1,6 @@
 // Верните массив в виде [x1, y1, x2, y2,..., xn, yn]
-var shuffle = function (nums, n) {};
+var shuffle = function (nums, n) {
+  return Array.from({ length: 2 * n }, (_, i) =>
+    i % 2 === 0 ? nums[i / 2] : nums[n + (i - 1) / 2],
+  );
+};
