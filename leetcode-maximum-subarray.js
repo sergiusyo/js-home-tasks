e0b2ab452ed0ba5237847ba0671d8821eabe0d35
Сергей Савин <sergius8482@gmail.com>
2026-07-04 11:31:27 +0300

    leetcode-maximum-subarray

--- leetcode-maximum-subarray.js
+++ leetcode-maximum-subarray.js
@@ -1,2 +1,12 @@
 // найти подмассив с наибольшей суммой и вернуть его сумму
-var maxSubArray = function (nums) {};
+var maxSubArray = function (n) {
+  let maxSum = n[0];
+
+  n.reduce((currentSum, num) => {
+    currentSum = Math.max(num, currentSum + num);
+    maxSum = Math.max(maxSum, currentSum);
+    return currentSum;
+  });
+
+  return maxSum;
+};
