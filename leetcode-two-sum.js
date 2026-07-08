4fd7b5fdd7b5e0a2c2972ba67bcfad33fd97bb62
Сергей Савин <sergius8482@gmail.com>
2026-07-08 16:27:36 +0300

    leetcode-two-sum

--- leetcode-two-sum.js
+++ leetcode-two-sum.js
@@ -1,24 +1,24 @@
 // Найти два числа в отсортированном массиве numbers, которые в сумме дают target.
-// var twoSum = function (nums, target) {
-//   // итерируется каждый элемент массива
-//   for (let i = 0; i < nums.length; i++) {
-//     // j - Индекс второго элемента. Проверяются все последующие элементы
-//     for (let j = i + 1; j < nums.length; j++) {
-//       // проверяется, равна ли сумма элементов с индексами i и j значению target
-//       if (nums[i] + nums[j] === target) {
-//         return [i, j]; // Возвращаем индексы найденной пары == true. Функция возвращает [0, 1]
-//       }
-//     }
-//   }
-//   return null;
-// };
+var twoSum = function (nums, target) {
+  // итерируется каждый элемент массива
+  for (let i = 0; i < nums.length; i++) {
+    // j - Индекс второго элемента. Проверяются все последующие элементы
+    for (let j = i + 1; j < nums.length; j++) {
+      // проверяется, равна ли сумма элементов с индексами i и j значению target
+      if (nums[i] + nums[j] === target) {
+        return [i, j]; // Возвращаем индексы найденной пары == true. Функция возвращает [0, 1]
+      }
+    }
+  }
+  return null;
+};
 
-// // Входные данные
-// const nums = [2, 7, 11, 15];
-// const target = 9;
+// Входные данные
+const nums = [2, 7, 11, 15];
+const target = 9;
 
-// const result = twoSum(nums, target);
-// console.log(result);
+const result = twoSum(nums, target);
+console.log(result);
 
 // Решение с использованием хэш-таблицы
 var twoSum = function (nums1, target1) {
