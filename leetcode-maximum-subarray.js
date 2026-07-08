2f88b926cca5a13fc87b59a257e890031f6f0ddc
Сергей Савин <sergius8482@gmail.com>
2026-07-08 16:54:53 +0300

    leetcode-maximum-subarray

--- leetcode-maximum-subarray.js
+++ leetcode-maximum-subarray.js
@@ -1,19 +1,19 @@
-// найти подмассив с наибольшей суммой и вернуть его сумму
-var maxSubArray = function (nums) {
-  // инициализируется первым элементом массива
-  let maxSum = nums[0];
+// // найти подмассив с наибольшей суммой и вернуть его сумму
+// var maxSubArray = function (nums) {
+//   // инициализируется первым элементом массива
+//   let maxSum = nums[0];
 
-  // проходит по каждому элементу массива (начиная со второго, т.к. первый уже учтен)
-  nums.reduce((currentSum, num) => {
-    // currentSum — текущая сумма подмассива, заканчивающегося на текущем элементе
-    currentSum = Math.max(num, currentSum + num);
-    // обновляем глобальный массив
-    maxSum = Math.max(maxSum, currentSum);
-    return currentSum;
-  });
+//   // проходит по каждому элементу массива (начиная со второго, т.к. первый уже учтен)
+//   nums.reduce((currentSum, num) => {
+//     // currentSum — текущая сумма подмассива, заканчивающегося на текущем элементе
+//     currentSum = Math.max(num, currentSum + num);
+//     // обновляем глобальный массив
+//     maxSum = Math.max(maxSum, currentSum);
+//     return currentSum;
+//   });
 
-  return maxSum;
-};
+//   return maxSum;
+// };
 
-const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-console.log(maxSubArray(nums));
+// const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+// console.log(maxSubArray(nums));
