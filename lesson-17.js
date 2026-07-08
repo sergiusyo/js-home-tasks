19989133d70aa870cfe04a5ea9d2e067a59244d2
Сергей Савин <sergius8482@gmail.com>
2026-07-08 17:05:19 +0300

    lesson-17

--- lesson-17.js
+++ lesson-17.js
@@ -15,3 +15,24 @@ console.log(square(4));
 console.log(square(7));
 
 // max(a, b)
+function max(a, b) {
+  let result = a > b ? a : b;
+  return result;
+}
+
+// console.log(max(3, 7));
+// console.log(max(10, 5));
+// console.log(max(4, 4));
+
+// isEven(n)
+function isEven(n) {
+  if (n % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(isEven(4));
+// console.log(isEven(7));
+// console.log(isEven(0));
