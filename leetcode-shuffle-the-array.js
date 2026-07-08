973f24d3109f1104aa24a0f12f7b8238727526c3
Сергей Савин <sergius8482@gmail.com>
2026-07-08 16:27:12 +0300

    leetcode-shuffle-the-array

--- leetcode-shuffle-the-array.js
+++ leetcode-shuffle-the-array.js
@@ -23,16 +23,16 @@ const n = 3;
 
 console.log(shuffle(nums2, n)); // 2, 3, 5, 4, 1, 7
 
-// var shuffle = function (nums2, n) {
-//   let result = [];
-//   for (let i = 0; i < n; i++) {
-//     result.push(nums2[i]); // берем из первой половины
-//     result.push(nums2[i + n]); // берем из второй половины
-//   }
-//   return result;
-// };
+var shuffle = function (nums2, n) {
+  let result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(nums2[i]); // берем из первой половины
+    result.push(nums2[i + n]); // берем из второй половины
+  }
+  return result;
+};
 
-// const nums2 = [2, 5, 1, 3, 4, 7];
-// const n = 3;
+const nums2 = [2, 5, 1, 3, 4, 7];
+const n = 3;
 
-// console.log(shuffle(nums2, n)); // 2, 3, 5, 4, 1, 7
+console.log(shuffle(nums2, n)); // 2, 3, 5, 4, 1, 7
