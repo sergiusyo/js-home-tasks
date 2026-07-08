6ea5bdd92c2261fb943a2b140e53ab326db69176
Сергей Савин <sergius8482@gmail.com>
2026-07-08 16:56:28 +0300

    lesson-17

--- lesson-17.js
+++ lesson-17.js
@@ -1,1 +1,17 @@
 // Функции. greet(name)
+function greet(name) {
+  console.log(`Привет, ${name}!`);
+}
+
+greet("Аня");
+greet("Вася");
+
+// square(n)
+function square(n) {
+  return n ** 2;
+}
+
+console.log(square(4));
+console.log(square(7));
+
+// max(a, b)
