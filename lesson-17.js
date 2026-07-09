fb3f92a9d910a72a7ebaf6581bd081e44decb50b
Сергей Савин <sergius8482@gmail.com>
2026-07-09 13:35:25 +0300

    lesson-17

--- lesson-17.js
+++ lesson-17.js
@@ -1,37 +1,37 @@
 // Функции. greet(name)
-function greet(name) {
-  console.log(`Привет, ${name}!`);
-}
+// function greet(name) {
+//   console.log(`Привет, ${name}!`);
+// }
 
-greet("Аня");
-greet("Вася");
+// greet("Аня");
+// greet("Вася");
 
 // square(n)
-function square(n) {
-  return n ** 2;
-}
+// function square(n) {
+//   return n ** 2;
+// }
 
-console.log(square(4));
-console.log(square(7));
+// console.log(square(4));
+// console.log(square(7));
 
 // max(a, b)
-function max(a, b) {
-  let result = a > b ? a : b;
-  return result;
-}
+// function max(a, b) {
+//   let result = a > b ? a : b;
+//   return result;
+// }
 
 // console.log(max(3, 7));
 // console.log(max(10, 5));
 // console.log(max(4, 4));
 
 // isEven(n)
-function isEven(n) {
-  if (n % 2 === 0) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function isEven(n) {
+//   if (n % 2 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 // console.log(isEven(4));
 // console.log(isEven(7));
