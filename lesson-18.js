7d5a8dcf75e9a01c07f11adb01bab60c588fd371
Сергей Савин <sergius8482@gmail.com>
2026-07-09 13:34:12 +0300

    lesson-18

--- lesson-18.js
+++ lesson-18.js
@@ -1,1 +1,11 @@
-//
+// Перепиши функцию
+function add(a, b) {
+  return a + b; // обычная функция
+}
+
+const add2 = (a, b) => {
+  return a + b;
+};
+
+console.log(add(3, 7)); // 10
+console.log(add2(3, 7)); // 10
