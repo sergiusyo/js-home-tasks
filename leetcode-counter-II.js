f8b90d39c366f36b7b884ac6496e7a30af53d582
Сергей Савин <sergius8482@gmail.com>
2026-07-25 13:01:24 +0300

    leetcode-counter-II

--- leetcode-counter-II.js
+++ leetcode-counter-II.js
@@ -15,7 +15,7 @@ var createCounter = function (init) {
   };
 };
 
-const counter = createCounter(5);
-console.log(counter.increment()); // 6
-console.log(counter.decrement()); // 5
-console.log(counter.reset()); // 5  (сброс к 5)
+// const counter = createCounter(5);
+// console.log(counter.increment()); // 6
+// console.log(counter.decrement()); // 5
+// console.log(counter.reset()); // 5  (сброс к 5)
