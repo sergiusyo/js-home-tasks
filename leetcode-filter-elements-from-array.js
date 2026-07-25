a93a7b4f1a3b010b51b67012cfad649280b8b107
Сергей Савин <sergius8482@gmail.com>
2026-07-25 17:07:28 +0300

    leetcode-filter-elements-from-array

--- leetcode-filter-elements-from-array.js
+++ leetcode-filter-elements-from-array.js
@@ -1,1 +1,12 @@
-var filter = function (arr, fn) {};
+var filter = function (arr, fn) {
+  const result = [];
+  let index = 0;
+  // value получает само значение элемента, а не его индекс
+  for (const value of arr) {
+    if (fn(value, index, arr)) {
+      result.push(value);
+    }
+    index++;
+  }
+  return result;
+};
