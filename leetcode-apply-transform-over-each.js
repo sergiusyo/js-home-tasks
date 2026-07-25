38aa72b6002d8ae56dc2237dd7731766790ae026
Сергей Савин <sergius8482@gmail.com>
2026-07-25 16:52:17 +0300

    leetcode-apply-transform-over-each.js

--- leetcode-apply-transform-over-each.js
+++ leetcode-apply-transform-over-each.js
@@ -1,12 +1,18 @@
+const arr = [1, 2, 3];
+
 var map = function (arr, fn) {
   const result = [];
-
   for (let i = 0; i < arr.length; i++) {
-    result.push(fn(arr[i], i));
+    result.push(fn(arr[i], i)); // fn — это функция, переданная как аргумент
   }
-
   return result;
 };
 
-const arr = [1, 2, 3];
-map(arr());
+// Передаем функцию x => x + 1
+const newArr = map(arr, (x) => x + 1);
+console.log(newArr); // 2, 3, 4
+
+// Принимает массив arr и функцию-преобразователь fn
+// Проходит по каждому элементу массива
+// Применяет fn к каждому элементу (передавая элемент и его индекс)
+// Возвращает новый массив с результатами
