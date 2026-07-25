acfc96339a45ee993f2b03ada6fcb7c58cd58438
Сергей Савин <sergius8482@gmail.com>
2026-07-25 13:21:21 +0300

    leetcode-to-be-or-not-to-be.js

---
+++ leetcode-to-be-or-not-to-be.js
@@ -0,0 +1,18 @@
+var expect = function (val) {
+  return {
+    toBe: function (expected) {
+      if (val === expected) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: function (expected) {
+      if (val !== expected) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+};
