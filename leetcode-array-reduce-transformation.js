dcdc5f15037d92f55ed3ae8e3b991b1dcd30c502
Сергей Савин <sergius8482@gmail.com>
2026-07-28 11:32:29 +0300

    leetcode-array-reduce-transformation

--- leetcode-array-reduce-transformation.js
+++ leetcode-array-reduce-transformation.js
@@ -1,1 +1,9 @@
-//
+var reduce = function (nums, fn, init) {
+  let result = init; // Начинаем с начального значения
+
+  for (let i = 0; i < nums.length; i++) {
+    result = fn(result, nums[i]); // Применяем fn к аккумулятору и текущему элементу
+  }
+
+  return result; // Возвращаем финальный результат
+};
