d9473453b6d7010d26d099b8deb0179fad0f6d56
Сергей Савин <sergius8482@gmail.com>
2026-08-07 14:00:31 +0300

    leetcode-two-sum-objects

--- leetcode-two-sum-objects.js
+++ leetcode-two-sum-objects.js
@@ -7,7 +7,13 @@ var twoSum = function (nums, target) {
     const complement = target - currentNum; // число, которое нужно найти
 
     if (map.hasOwnProperty(complement)) {
-      return [map[complement], i];
+      // проверяется наличие ключа complement в map
+      return [map[complement], i]; // возвращаем индекс сохраненного числа и сам индекс
     }
+
+    map[currentNum] = i; // Если дополнение не найдено — запоминаем текущее число и его индекс
   }
+  return null;
 };
+
+console.log(twoSum([2, 7, 11, 15], 9));
