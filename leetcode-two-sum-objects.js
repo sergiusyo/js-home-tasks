2b0b1cf731420c89ac658c407abdd19ef6e6ac0a
Сергей Савин <sergius8482@gmail.com>
2026-08-07 13:49:42 +0300

    leetcode-two-sum-objects

--- leetcode-two-sum-objects.js
+++ leetcode-two-sum-objects.js
@@ -1,1 +1,13 @@
-// 
+// Найти в массиве nums два числа, которые в сумме дают target, и вернуть их индексы
+var twoSum = function (nums, target) {
+  const map = {}; // хранит уже просмотренные числа и их индексы
+
+  for (let i = 0; i < nums.length; i++) {
+    const currentNum = nums[i]; // число на текущей позиции
+    const complement = target - currentNum; // число, которое нужно найти
+
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i];
+    }
+  }
+};
