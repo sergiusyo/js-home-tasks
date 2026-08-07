4464291855c222e19e6e0ac59037c501cf978514
Сергей Савин <sergius8482@gmail.com>
2026-08-07 14:25:59 +0300

    leetcode-ransom-note

--- leetcode-ransom-note.js
+++ leetcode-ransom-note.js
@@ -1,2 +1,14 @@
-//
-var canConstruct = function (ransomNote, magazine) {};
+// 🧠
+var canConstruct = function (ransomNote, magazine) {
+  let magArray = magazine.split(""); // разбиваем строку magazine на массив отдельных символов.
+  for (let char of ransomNote) {
+    const index = magArray.indexOf(char); // ищем текущую букву char в массиве журнала. indexOf() возвращает:
+    // Индекс первого найденного совпадения. -1, если буква не найдена
+    if (index === -1) {
+      return false; // Если буквы нет в журнале → записку составить невозможно.
+    }
+    // Удаляем найденную букву, чтобы не использовать её повторно
+    magArray.splice(index, 1);
+  }
+  return true;
+};
