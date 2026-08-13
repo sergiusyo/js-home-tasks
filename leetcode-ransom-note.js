1be37488d6e11baf1357de852174eb112eba5848
Сергей Савин <sergius8482@gmail.com>
2026-08-13 17:56:12 +0300

    leetcode-ransom-note

--- leetcode-ransom-note.js
+++ leetcode-ransom-note.js
@@ -2,7 +2,7 @@
 var canConstruct = function (ransomNote, magazine) {
   let magArray = magazine.split(""); // разбиваем строку magazine на массив отдельных символов.
   for (let char of ransomNote) {
-    const index = magArray.indexOf(char); // ищем текущую букву char в массиве журнала. indexOf() возвращает:
+    const index = magArray.indexOf(char); // Поиск буквы в журнале. indexOf() возвращает:
     // Индекс первого найденного совпадения. -1, если буква не найдена
     if (index === -1) {
       return false; // Если буквы нет в журнале → записку составить невозможно.
