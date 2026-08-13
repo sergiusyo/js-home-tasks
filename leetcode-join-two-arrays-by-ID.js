cf0f96209b71e8e4bf6d77fbcb7cd2ad1aff860f
Сергей Савин <sergius8482@gmail.com>
2026-08-13 18:08:37 +0300

    leetcode-join-two-arrays-by-ID

--- leetcode-join-two-arrays-by-ID.js
+++ leetcode-join-two-arrays-by-ID.js
@@ -21,3 +21,13 @@ var join = function (arr1, arr2) {
   // Преобразуем объект обратно в массив и сортируем по id
   return Object.values(merged).sort((a, b) => a.id - b.id);
 };
+
+// const arr1 = [
+//   { id: 1, x: 1 },
+//   { id: 2, x: 9 },
+// ];
+
+// const arr2 = [{ id: 3, x: 5 }];
+
+// const result = join(arr1, arr2);
+// console.log(result);
