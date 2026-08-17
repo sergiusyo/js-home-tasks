80e62773c6a1dc34b110ed3e1e1f5151113069e5
Сергей Савин <sergius8482@gmail.com>
2026-08-17 11:46:29 +0300

    prototypes-classes

--- prototypes-classes.js
+++ prototypes-classes.js
@@ -5,10 +5,26 @@ class Person {
     this.age = age;
   }
 
-  speak() {
+  introduce() {
     return `Меня зовут ${this.name}, мне ${this.age} лет`;
   }
 }
 
 const girl = new Person("Анна", 20);
-console.log(girl.speak());
+console.log(girl.introduce());
+
+// Класс Student
+class Student extends Person {
+  constructor(name, age, faculty) {
+    super(name, age)
+    this.faculty = faculty
+  }
+  introduce() {
+    const base = super.introduce()
+    return `${base}. Факультет: ${this.faculty}`
+  }
+}
+
+const ivan = new Student('Иван', 18, 'Математика')
+console.log(ivan.introduce())
+// Меня зовут Иван, мне 18 лет. Факультет: Математика
